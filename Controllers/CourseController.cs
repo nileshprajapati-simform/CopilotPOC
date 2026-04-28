539abd8772577330304e9bbdaa75cf3d0a6eabaa
Nilesh Prajapati <102572738+nileshprajapati-simform@users.noreply.github.com>
2026-04-28 10:43:23 +0530

    Remove redundant GetAllCourses method from CourseController

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -23,12 +23,7 @@ public class CourseController : ControllerBase
         var courses = await _courseService.GetAllAsync();
         return Ok(courses);
     }
-    [HttpGet]
-    public async Task<IActionResult> GetAllCourses()
-    {
-        var courses = await _courseService.GetAllAsync();
-        return Ok(courses);
-    }
+    
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(int id)
